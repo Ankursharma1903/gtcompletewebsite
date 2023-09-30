6443903eb3301b7326a128d21272b490e6f9a733
ankur <ankur.as502@gmail.com>
2023-09-30 16:58:33 +0530

    ready to host on vercel added vercel and vercel.json

--- app.js
+++ app.js
@@ -1,82 +1,82 @@
-
-var express = require('express');
+var express = require("express");
 var app = express();
 var router = express.Router();
-const bodyparser=require("body-parser");
+const bodyparser = require("body-parser");
 
-  
-var path = __dirname + '/views/';
-  
-app.use('/',router);
-app.use('/static',express.static('static'))
-app.use(express.urlencoded())
+var path = __dirname + "/views/";
+
+app.use("/", router);
+app.use("/static", express.static("static"));
+app.use(express.urlencoded());
 // connecting mongoose
-const mongoose = require('mongoose');
-const { stringify } = require('nodemon/lib/utils');
+const mongoose = require("mongoose");
+const { stringify } = require("nodemon/lib/utils");
 
-mongoose.connect('mongodb://localhost/gamingtitans',{useNewUrlParser:true});
+// mongoose.connect('mongodb://localhost/gamingtitans',{useNewUrlParser:true});
+mongoose.connect(`${process.env.MONGODB_CONNECT_URI}`, {
+  useNewUrlParser: true,
+});
 var db = mongoose.connection;
 // this will make connection
-db.on('error', console.error.bind(console,'connection error:'));
-db.once('open',function(){
-    // we are connected !
-    console.log("we are connected bro....")
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  // we are connected !
+  console.log("we are connected bro....");
 });
 // define mongoose schema
 var contactSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    Phone: String,
-    Branch: String,
-    Section: String,
-    Enrollment: String,
-    Year: String,
-    Game: String,
-    subject:String,
-    message: String
-  });
-  var contact = mongoose.model('contact', contactSchema);
+  name: String,
+  email: String,
+  Phone: String,
+  Branch: String,
+  Section: String,
+  Enrollment: String,
+  Year: String,
+  Game: String,
+  subject: String,
+  message: String,
+});
+var contact = mongoose.model("contact", contactSchema);
 // it likely same for mysql
 //  mpm i body-parser is must to take input
 
-
 // mongoose part ends
-  
-router.get('/',function(req, res){
-  res.sendFile(path + 'index.html');
+
+router.get("/", function (req, res) {
+  res.sendFile(path + "index.html");
 });
-  
-router.get('/read',function(req, res){
-  res.sendFile(path + 'read.html');});
-  
-router.get('/core-team',function(req, res){
-  res.sendFile(path + 'core-team.html');
+
+router.get("/read", function (req, res) {
+  res.sendFile(path + "read.html");
 });
-router.get('/contact',function(req,res){
-  
-   
-  res.sendFile(path + 'contact.html');
+
+router.get("/core-team", function (req, res) {
+  res.sendFile(path + "core-team.html");
+});
+router.get("/contact", function (req, res) {
+  res.sendFile(path + "contact.html");
 });
 // setting my form
 // router.post('/contact',function(req,res){
-app.post('/contact',function(req,res){
-  
+app.post("/contact", function (req, res) {
   var mydata = new contact(req.body);
-//   so when someone click we will form a new data
-mydata.save().then(()=>{
-    res.send("this item has been saved to the database")
-}).catch(()=>{
- res.status(400).send("item was not saved");
-})
+  //   so when someone click we will form a new data
+  mydata
+    .save()
+    .then(() => {
+      res.send("this item has been saved to the database");
+    })
+    .catch(() => {
+      res.status(400).send("item was not saved");
+    });
 });
 // form part ends
 
-
-app.use('*',function(req, res){
-  res.send('Error 404: Not Found!');
+app.use("*", function (req, res) {
+  res.send("Error 404: Not Found!");
 });
-  
-app.listen(3000,function(){
+PORT = process.env.PORT;
+app.listen(PORT, function () {
   console.log("Server running at Port 3000");
 });
 
@@ -88,7 +88,6 @@ app.listen(3000,function(){
 // show collections
 // we can use bootstrap alert
 
-
-// write in mongoose 
+// write in mongoose
 // db.contacts.find()
 // to see all the data stored in it
